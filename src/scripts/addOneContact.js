e17b96e7ba28f659c9669da88f2fd4a4fcc56891
juliya.lysenko <juliya.deleh@gmail.com>
2025-09-09 13:51:35 +0300

    commit1

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,12 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const data = await readContacts();
+  const newContacts = createFakeContact();
+  data.push(newContacts);
+  await writeContacts(data);
+};
 
 addOneContact();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts);
+  await fs.writeFile(PATH_DB, data, { encoding: 'utf-8' });
+};
